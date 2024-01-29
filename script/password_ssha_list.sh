a6a1f14939db7864db1f5a488cffda419863a352
João Pedro Ribeiro <61890653+joao-prs@users.noreply.github.com>
2024-01-29 14:11:54 -0300

    script de senha update

--- script/password_ssha_list.sh
+++ script/password_ssha_list.sh
@@ -8,6 +8,10 @@ read mypass
 PASSWORD=$mypass
 SALT="$(openssl rand -base64 3)"
 SHA1=$(printf "$PASSWORD$SALT" | openssl dgst -binary -sha1 | sed 's#$#'"$SALT"'#' | base64)
+
+echo "------------------"
+echo -e "hash da sua ${amarelo}senha${reset}:"
+echo "------------------"
 printf "{SSHA}$SHA1\n"
 
 # pegue a hash e envie para
