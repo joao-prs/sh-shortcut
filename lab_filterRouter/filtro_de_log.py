6c02d854f8ead45423c3c6005c222dd3a25a432f
jpedro <kandrocmatunein@gmail.com>
2024-02-19 08:19:21 -0300

    formalize texts

--- lab_filterRouter/filtro_de_log.py
+++ lab_filterRouter/filtro_de_log.py
@@ -2,15 +2,15 @@
 # Nome do arquivo de log original
 arquivo_original = 'arquivo_de_log.txt'
 
-# Nome do arquivo de log para o Mikrotik 1
-arquivo_mikrotik_1 = 'mikrotik_1_log.txt'
+# Nome do arquivo de log para o tik 1
+arquivo_tik_1 = 'tik_1_log.txt'
 
-# Nome do arquivo de log para o Mikrotik 2
-arquivo_mikrotik_2 = 'mikrotik_2_log.txt'
+# Nome do arquivo de log para o tik 2
+arquivo_tik_2 = 'tik_2_log.txt'
 
-# Números dos Mikrotiks
-mikrotik_1 = '54'
-mikrotik_2 = '56'
+# Números dos tiks
+tik_1 = '54'
+tik_2 = '56'
 
 # Função para extrair e deletar linhas específicas
 def extrair_e_deletar_linhas(origem, destino, identificador):
@@ -18,20 +18,20 @@ def extrair_e_deletar_linhas(origem, destino, identificador):
         linhas = arquivo_origem.readlines()
 
     # Filtra as linhas do identificador desejado
-    linhas_identificadas = [linha for linha in linhas if f'mikrotik{identificador}' in linha]
+    linhas_identificadas = [linha for linha in linhas if f'tik{identificador}' in linha]
 
     # Escreve as linhas identificadas no arquivo de destino
     with open(destino, 'a') as arquivo_destino:
         arquivo_destino.writelines(linhas_identificadas)
 
     # Remove as linhas identificadas do arquivo original
-    linhas_restantes = [linha for linha in linhas if f'mikrotik{identificador}' not in linha]
+    linhas_restantes = [linha for linha in linhas if f'tik{identificador}' not in linha]
     
     with open(origem, 'w') as arquivo_origem:
         arquivo_origem.writelines(linhas_restantes)
 
-# Extrai e deleta as linhas para o Mikrotik 1
-extrair_e_deletar_linhas(arquivo_original, arquivo_mikrotik_1, mikrotik_1)
+# Extrai e deleta as linhas para o tik 1
+extrair_e_deletar_linhas(arquivo_original, arquivo_tik_1, tik_1)
 
-# Extrai e deleta as linhas para o Mikrotik 2
-extrair_e_deletar_linhas(arquivo_original, arquivo_mikrotik_2, mikrotik_2)
+# Extrai e deleta as linhas para o tik 2
+extrair_e_deletar_linhas(arquivo_original, arquivo_tik_2, tik_2)
